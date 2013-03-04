0f1c8367065a8789f563ff2ad1098ad26aaa4713
Jon Sharkey <jon.sharkey@gmail.com>
2013-03-04 02:00:12 -0800

    Fudge touch movement to reduce perceived lag.

--- src/ios/GameTouchWindow.mm
+++ src/ios/GameTouchWindow.mm
@@ -55,6 +55,21 @@ using namespace std;
   for (UITouch *touch in touches) {
     Touch converted_touch;
     CGPoint location = [touch locationInView:touch.view];
+    CGPoint previous_location = [touch previousLocationInView:touch.view];
+
+    // Fudge the point by some factor of the velocity, to prevent the lag in movement.
+    double diff_x = location.x - previous_location.x;
+    double diff_y = location.y - previous_location.y;
+    double velocity_squared = diff_x * diff_x + diff_y * diff_y;
+    double factor = 0.5;
+    if (velocity_squared > 25*25) {
+      factor = 1.5;
+    } else if (velocity_squared > 14.43*14.43) {
+      factor = velocity_squared / 416.66;
+    }
+    location.x = location.x + diff_x * factor;
+    location.y = location.y + diff_y * factor;
+
     ScreenPoint l = screen_point_make(location.x * scale_, location.y * scale_);
     GamePoint p = gameEngine_->screen_point_to_game_point(l);
     converted_touch.set_location(p);
