05a9f0589a69d0649d6f420d0f7a36f832982a5d
Jon Sharkey <jon.sharkey@gmail.com>
2013-03-04 19:17:57 -0800

    Support zoom.

--- src/opengl/texture2d.cc
+++ src/opengl/texture2d.cc
@@ -99,6 +99,7 @@ void Texture2D::DrawAtPoint(ScreenPoint point, GLfloat alpha, GLfloat zoom, GLfl
   glTexCoordPointer(2, GL_FLOAT, 0, coordinates_);
   glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_COMBINE);
   glTranslatef(point.x + width/2.0, point.y - height/2.0, 0.0);
+  glScalef(zoom, zoom, 0);
   glRotatef(angle, 0.f, 0.f, 1.f);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
   glColor4f(1.f, 1.f, 1.f, alpha*globalAlpha_);
