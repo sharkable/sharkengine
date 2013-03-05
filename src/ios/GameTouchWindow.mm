cecb0eba4deb4b8351594715215318d64bef7862
Jon Sharkey <jon.sharkey@gmail.com>
2013-03-04 18:29:35 -0800

    Made the move speed fudging a bit more organic… and less jittery.

--- src/ios/GameTouchWindow.mm
+++ src/ios/GameTouchWindow.mm
@@ -22,6 +22,7 @@ using namespace std;
  @private
   GameEngine *gameEngine_;  // weak
   CGFloat scale_;
+  double move_factor_;
 }
 
 @synthesize gameEngine = gameEngine_;
@@ -37,6 +38,7 @@ using namespace std;
 #pragma mark - UIWindow
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+  move_factor_ = 0;
   gameEngine_->set_touches_began([self convertTouches:touches]);
 }
 
@@ -51,24 +53,26 @@ using namespace std;
 #pragma mark - Private
 
 - (vector<Touch>)convertTouches:(NSSet *)touches {
+  
   vector<Touch> converted_touches;
   for (UITouch *touch in touches) {
     Touch converted_touch;
     CGPoint location = [touch locationInView:touch.view];
-    CGPoint previous_location = [touch previousLocationInView:touch.view];
 
     // Fudge the point by some factor of the velocity, to prevent the lag in movement.
+    CGPoint previous_location = [touch previousLocationInView:touch.view];
     double diff_x = location.x - previous_location.x;
     double diff_y = location.y - previous_location.y;
+    double new_factor = 0;
     double velocity_squared = diff_x * diff_x + diff_y * diff_y;
-    double factor = 0.5;
-    if (velocity_squared > 25*25) {
-      factor = 1.5;
-    } else if (velocity_squared > 14.43*14.43) {
-      factor = velocity_squared / 416.66;
+    if (velocity_squared > 25 * 25) {
+      new_factor = 1.5;
+    } else {
+      new_factor = velocity_squared / (25 * 25 / 1.5);
     }
-    location.x = location.x + diff_x * factor;
-    location.y = location.y + diff_y * factor;
+    move_factor_ = (move_factor_ + new_factor) / 2;
+    location.x = location.x + diff_x * move_factor_;
+    location.y = location.y + diff_y * move_factor_;
 
     ScreenPoint l = screen_point_make(location.x * scale_, location.y * scale_);
     GamePoint p = gameEngine_->screen_point_to_game_point(l);
