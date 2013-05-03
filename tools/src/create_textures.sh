0996e7b05cfe1795afc1b871c47d736a97fb8dac
Jon Sharkey <jon.sharkey@gmail.com>
2013-05-02 20:15:25 -0700

    Remove directory structure for resources/textures (from create_textures script.)

--- tools/src/create_textures.sh
+++ tools/src/create_textures.sh
@@ -2,6 +2,5 @@
 
 # Run this from the artresources folder to create tx files from png texture files.
 
-eval $( find textures | grep png$ | \
-    sed 's|\(.*\)png|../gameengine/tools/bin/png2tx \1png ../resources/\1tx;|' );
-    
+eval $( find textures | grep png$ | sed \
+    's|\(.*/\)\(.*\)png|$SHARKENGINE_HOME/tools/bin/png2tx \1\2png ../resources/textures/\2tx;|' );
