78209ad97fe0b78f1114857d2e0b00ee7cca818a
Jon Sharkey <jon.sharkey@gmail.com>
2013-07-31 18:54:19 -0400

    [iOS] Project file contains all of the Sharkengine code, to fix issues with Apperater. Only working for Debug right now.
    Issuers were:
    - Appirater.m needs ARC. Having it start there lets us add this as a
    build parameter.
    - Strings files need to be added IN Xcode to be added correctly.

--- platforms/ios/build/setup_project.py
+++ platforms/ios/build/setup_project.py
@@ -19,10 +19,4 @@ app_group = project.get_or_create_group('App')
 project.add_folder(project_home + '/src', app_group)
 project.add_folder(project_home + '/resources', app_group)
 
-shark_engine_group = project.get_or_create_group('SharkEngine')
-project.add_folder(sharkengine_home + '/core', shark_engine_group)
-
-shark_engine_ios_group = project.get_or_create_group('iOS')
-project.add_folder(sharkengine_home + '/platforms/ios/src', shark_engine_ios_group)
-
 project.saveFormat3_2()
