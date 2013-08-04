54af930109dc60383bc341c1e71813546da38d8e
Jon Sharkey <jon.sharkey@gmail.com>
2013-08-04 13:30:21 -0400

    [Apple] Use app.config vars in Info.plist.

--- platforms/ios/build/build.sh
+++ platforms/ios/build/build.sh
@@ -1,6 +1,15 @@
 mkdir -p $SHARKENGINE_APP_HOME/out/ios/SharkengineApp.xcodeproj
+
 sed "s/___SHARKENGINE_HOME___/$(echo $SHARKENGINE_HOME | sed -e 's/[\/&]/\\&/g')/g" \
   < $SHARKENGINE_HOME/platforms/ios/build/SharkengineApp.xcodeproj/project.pbxproj \
   > $SHARKENGINE_APP_HOME/out/ios/SharkengineApp.xcodeproj/project.pbxproj
+
 cp $SHARKENGINE_HOME/platforms/ios/build/Info.plist $SHARKENGINE_APP_HOME/out/ios
+
+VERSION=$VERSION_MAJOR.$VERSION_MINOR
+sed -e "s/___SHARKENGINE_APP_VERSION___/$VERSION/g" \
+    -e "s/___SHARKENGINE_APP_NAME___/$APP_NAME/g" \
+    < $SHARKENGINE_HOME/platforms/ios/build/Info.plist \
+    > $SHARKENGINE_APP_HOME/out/ios/Info.plist
+
 python $SHARKENGINE_HOME/platforms/ios/build/setup_project.py
