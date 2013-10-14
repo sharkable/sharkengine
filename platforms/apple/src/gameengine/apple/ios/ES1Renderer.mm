59ee6737c8aa06f8a87b3acbdce9c1ffbdb7fe21
Jon Sharkey <jon.sharkey@gmail.com>
2013-10-13 22:18:49 -0400

    Correctly set the size of the depth buffer.

--- platforms/apple/src/gameengine/apple/ios/ES1Renderer.mm
+++ platforms/apple/src/gameengine/apple/ios/ES1Renderer.mm
@@ -24,47 +24,6 @@
   GLuint depthRenderbuffer_;
 }
 
-// Create an OpenGL ES 1.1 context
-- (id)init {
-  self = [super init];
-  if (self) {
-    context_ = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES1];
-
-    if (!context_ || ![EAGLContext setCurrentContext:context_]) {
-        [self release];
-        return nil;
-    }
-
-    // Create default framebuffer object. The backing will be allocated for the current layer in
-    // -resizeFromLayer
-    glGenFramebuffersOES(1, &defaultFramebuffer_);
-    glGenRenderbuffersOES(1, &colorRenderbuffer_);
-    glBindFramebufferOES(GL_FRAMEBUFFER_OES, defaultFramebuffer_);
-    glBindRenderbufferOES(GL_RENDERBUFFER_OES, colorRenderbuffer_);
-    glFramebufferRenderbufferOES(GL_FRAMEBUFFER_OES, GL_COLOR_ATTACHMENT0_OES, GL_RENDERBUFFER_OES,
-                                 colorRenderbuffer_);
-
-    // TODO how do I do this properly?
-    glGenRenderbuffers(1, &depthRenderbuffer_);
-    glBindRenderbuffer(GL_RENDERBUFFER, depthRenderbuffer_);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, 1136, 640);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderbuffer_);
-
-
-    glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
-    glEnable(GL_CULL_FACE);
-    glEnable(GL_BLEND);
-    glEnable(GL_TEXTURE_2D);
-    glEnable(GL_POLYGON_OFFSET_FILL);
-    glDisable(GL_DEPTH_TEST);
-    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-  }
-
-  return self;
-}
-
 - (void)dealloc {
   // Tear down GL
   if (defaultFramebuffer_) {
@@ -108,12 +67,49 @@
 }
 
 - (BOOL)resizeFromLayer:(CAEAGLLayer *)layer {
+  context_ = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES1];
+
+  if (!context_ || ![EAGLContext setCurrentContext:context_]) {
+    [self release];
+    return nil;
+  }
+
+  // Create default framebuffer object. The backing will be allocated for the current layer in
+  // -resizeFromLayer
+  glGenFramebuffersOES(1, &defaultFramebuffer_);
+  glGenRenderbuffersOES(1, &colorRenderbuffer_);
+  glBindFramebufferOES(GL_FRAMEBUFFER_OES, defaultFramebuffer_);
+  glBindRenderbufferOES(GL_RENDERBUFFER_OES, colorRenderbuffer_);
+  glFramebufferRenderbufferOES(GL_FRAMEBUFFER_OES, GL_COLOR_ATTACHMENT0_OES, GL_RENDERBUFFER_OES,
+                               colorRenderbuffer_);
+
+  // TODO how do I do this properly? I can't make this happen AFTER the next section (to get the
+  // width and heigh THAT way.)
+  CGSize layerSize = layer.visibleRect.size;
+  float scale = [UIScreen mainScreen].scale;
+  glGenRenderbuffers(1, &depthRenderbuffer_);
+  glBindRenderbuffer(GL_RENDERBUFFER, depthRenderbuffer_);
+  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, layerSize.width * scale,
+                        layerSize.height * scale);
+  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER,
+                            depthRenderbuffer_);
+
   // Allocate color buffer backing based on the current layer size
   glBindRenderbufferOES(GL_RENDERBUFFER_OES, colorRenderbuffer_);
   [context_ renderbufferStorage:GL_RENDERBUFFER_OES fromDrawable:layer];
   glGetRenderbufferParameterivOES(GL_RENDERBUFFER_OES, GL_RENDERBUFFER_WIDTH_OES, &backingWidth_);
   glGetRenderbufferParameterivOES(GL_RENDERBUFFER_OES, GL_RENDERBUFFER_HEIGHT_OES, &backingHeight_);
   Texture2D::SetScreenHeight(backingHeight_);
+
+  glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+  glEnable(GL_CULL_FACE);
+  glEnable(GL_BLEND);
+  glEnable(GL_TEXTURE_2D);
+  glEnable(GL_POLYGON_OFFSET_FILL);
+  glDisable(GL_DEPTH_TEST);
+  glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+  glEnableClientState(GL_VERTEX_ARRAY);
+  glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 
   if (glCheckFramebufferStatusOES(GL_FRAMEBUFFER_OES) != GL_FRAMEBUFFER_COMPLETE_OES) {
     NSLog(@"Failed to make complete framebuffer object %x",
