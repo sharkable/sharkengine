2b3b0edee07820241f83be5c67a91c798479fce2
Jon Sharkey <jon.sharkey@gmail.com>
2013-12-29 18:13:39 -0500

    Added ‘GroupSimulator’.

---
+++ core/src/gameengine/simulation/group_simulator.h
@@ -0,0 +1,38 @@
+//
+//  group_simulator.h
+//  GameEngine
+//
+//  Created by Jon Sharkey on 2013-12-29.
+//  Copyright 2013 Sharkable. All rights reserved.
+//
+
+#ifndef GAMEENGINE_SIMULATION_GROUPSIMULATOR_H_
+#define GAMEENGINE_SIMULATION_GROUPSIMULATOR_H_
+
+#include "gameengine/datastructures/staged_vector.h"
+
+#include "gameengine/simulation/simulator.h"
+
+class GroupSimulator : public Simulator {
+ public:
+  void AddSimulator(Simulator *simulator) {
+    simulators_.StagePushBack(simulator);
+  }
+
+  void RemoveSimulator(Simulator *simulator) {
+    simulators_.StageErase(simulator);
+  }
+
+  // Simulator
+  virtual void SimulateStep() {
+    simulators_.CommitStaging();
+    for (auto i = simulators_.Begin(); i != simulators_.End(); ++i) {
+      (*i)->SimulateStep();
+    }
+  }
+
+ private:
+  StagedVector<Simulator> simulators_;
+};
+
+#endif
